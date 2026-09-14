004319fe7e68ee4080471609e7dc950d3a82fab1
juanmumya <Yildiz.453@icloud.com>
2026-09-14 23:42:46 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,96 @@
+const CACHE_NAME = "disiplinli-harcama-v4";
+
+const FILES = [
+    "./",
+    "./HTML",
+    "./manifest.json"
+];
+
+
+self.addEventListener(
+    "install",
+    function(event) {
+
+        event.waitUntil(
+
+            caches
+                .open(CACHE_NAME)
+                .then(
+                    function(cache) {
+
+                        return cache.addAll(
+                            FILES
+                        );
+
+                    }
+                )
+
+        );
+
+        self.skipWaiting();
+
+    }
+);
+
+
+self.addEventListener(
+    "activate",
+    function(event) {
+
+        event.waitUntil(
+
+            caches
+                .keys()
+                .then(
+                    function(names) {
+
+                        return Promise.all(
+
+                            names
+                                .filter(
+                                    function(name) {
+                                        return name !== CACHE_NAME;
+                                    }
+                                )
+                                .map(
+                                    function(name) {
+                                        return caches.delete(name);
+                                    }
+                                )
+
+                        );
+
+                    }
+                )
+
+        );
+
+        self.clients.claim();
+
+    }
+);
+
+
+self.addEventListener(
+    "fetch",
+    function(event) {
+
+        event.respondWith(
+
+            caches
+                .match(event.request)
+                .then(
+                    function(response) {
+
+                        return (
+                            response ||
+                            fetch(event.request)
+                        );
+
+                    }
+                )
+
+        );
+
+    }
+);
